af7f9445d8600c3511936d79c70ea80f3234eb17
DiegoBarra97 <diegobarra9797@gmail.com>
2023-06-20 18:04:48 -0300

    feat: abrir a página de consultas com as contas do mês atual

--- src/main/java/br/com/cotiinformatica/controllers/ContasConsultaController.java
+++ src/main/java/br/com/cotiinformatica/controllers/ContasConsultaController.java
@@ -1,33 +1,57 @@
 package br.com.cotiinformatica.controllers;
-
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.TemporalAdjusters;
 import java.util.Date;
 import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
 import br.com.cotiinformatica.dtos.ContasConsultaDto;
 import br.com.cotiinformatica.entities.Conta;
 import br.com.cotiinformatica.entities.Usuario;
 import br.com.cotiinformatica.repositories.ContaRepository;
-
 @Controller
 public class ContasConsultaController {
 	
 	@Autowired
 	private ContaRepository contaRepository;
-
 	@RequestMapping(value = "/admin/contas-consulta")
-	public ModelAndView contasConsulta() {
+	public ModelAndView contasConsulta(HttpServletRequest request) {
 		// WEB-INF/views/admin/contas-consulta.jsp
 		ModelAndView modelAndView = new ModelAndView("admin/contas-consulta");
-		modelAndView.addObject("dto", new ContasConsultaDto());
+		ContasConsultaDto dto = new ContasConsultaDto();
+		
+		try {
+			//capturando a data de inicio e fim do mês atual..
+			LocalDate primeiroDiaDoMes = LocalDate.now().with(TemporalAdjusters.firstDayOfMonth());
+			LocalDate ultimoDiaDoMes = LocalDate.now().with(TemporalAdjusters.lastDayOfMonth());
+			
+			//capturar o usuário autenticado no sistema (sessão)
+			Usuario usuario = (Usuario) request.getSession().getAttribute("auth_usuario");
+			
+			//converter as datas para java.util.Date
+			Date dataInicio = Date.from(primeiroDiaDoMes.atStartOfDay(ZoneId.systemDefault()).toInstant());
+			Date dataFim = Date.from(ultimoDiaDoMes.atStartOfDay(ZoneId.systemDefault()).toInstant());
+			
+			//realizando a consulta no banco de dados
+			List<Conta> contas = contaRepository.findAll(dataInicio, dataFim, usuario.getIdUsuario());
+			
+			//enviando o resltado da busca para a página
+			modelAndView.addObject("contas", contas);
+			
+			dto.setDataInicio(new SimpleDateFormat("yyyy-MM-dd").format(dataInicio));
+			dto.setDataFim(new SimpleDateFormat("yyyy-MM-dd").format(dataFim));
+		}
+		catch(Exception e) {
+			modelAndView.addObject("mensagem", e.getMessage());
+		}
+		
+		modelAndView.addObject("dto", dto);
 		return modelAndView;
 	}
 	
@@ -89,3 +113,4 @@ public class ContasConsultaController {
 	}
 	
 }
+
